d763004ef9f819a3deb45b48150c741e14a2000e
manisha.h.sharma <manisha.h.sharma@in.ey.com>
2020-08-04 14:35:52 +0530

    deleted files

--- src/main/resources/data.sql
+++
@@ -1,5 +0,0 @@
-INSERT INTO  role values(100,'Admin');
-INSERT INTO  role values(101,'User');
-INSERT INTO  role values(102,'Developer');
-INSERT INTO  role values(103,'HOD');
-INSERT INTO  role values(104,'CP');
